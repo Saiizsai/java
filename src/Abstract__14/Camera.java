fa5046ae26260f8de5194d05f06b7a896203f2f5
Saiizsai <saihlaingphyoaung03@gmail.com>
2022-06-02 16:09:25 +0630

    oop lessons

---
+++ src/Abstract__14/Camera.java
@@ -0,0 +1,11 @@
+package Abstract__14;
+
+public class Camera extends Machine{
+    @Override
+    public void start() {
+        System.out.println("camera started");
+    }
+    public void stop() {
+        System.out.println("camera stopped");
+    }
+}
---
+++ src/One.java
@@ -0,0 +1,9 @@
+public class One extends Main {
+    static int amount ;
+int a= 10;
+    public void r (int amt) {
+        amount = amount + amt;
+        System.out.println("you amount is " + amt);
+    }
+
+}
