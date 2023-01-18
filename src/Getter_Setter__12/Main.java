fe1b7f9d0ff8e0b28bac1f738ef519fed9162308
sai <saihlaingphyoaung03@gmail.com>
2023-01-18 13:20:12 +0630

    Revert "Revert "Revert "Revert "Revert "oop lessons"""""
    This reverts commit 95b680c10e49801b3fa78dc5ede3181dbb2982e0.

--- src/Getter_Setter__12/Main.java
+++ src/Getter_Setter__12/Main.java
@@ -1,11 +1,2 @@
-package Getter_Setter__12;
-
-public class Main {
-    public static void main(String[] args) {
-        Student sd = new Student();
-//        sd.id = 12;
-        sd.setId(100);
-        System.out.println(sd.getId());
-
-    }
+package Getter_Setter__12;public class Main {
 }
