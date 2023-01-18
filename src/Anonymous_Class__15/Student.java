b5c147ea31b382e42b27f1ba9154236bcabdd8ad
sai <saihlaingphyoaung03@gmail.com>
2023-01-18 13:21:36 +0630

    Revert "Revert "Revert "Revert "Revert "Revert "Revert "Revert "Revert "Revert "Revert "Revert "Revert "Revert "Revert "Revert "Revert "oop lessons"""""""""""""""""
    This reverts commit 635c9560cdb68d6a357433400450a44ef194ddcd.

--- src/Anonymous_Class__15/Student.java
+++
@@ -1,8 +0,0 @@
-package Anonymous_Class__15;
-
-public class Student extends Person{
-    @Override
-    public void greet() {
-        System.out.println("Hello I'm Student class");
-    }
-}
--- src/SuperKeyword__16/Child.java
+++
@@ -1,9 +0,0 @@
-package SuperKeyword__16;
-
-public class Child extends Parent{
-    static String s = "this is child";
-    void show() {
-        System.out.println(super.s);
-    }
-
-}
