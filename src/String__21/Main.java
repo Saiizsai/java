2f304c0fc59e50907b60e7ff3d6c4ab69d8eee96
sai <saihlaingphyoaung03@gmail.com>
2023-01-18 13:20:21 +0630

    Revert "Revert "Revert "Revert "Revert "Revert "Revert "oop lessons"""""""
    This reverts commit a15108f9a2cf4d5df95f9fb6cee7e6c4b38e6bfb.

--- src/String__21/Main.java
+++
@@ -1,9 +0,0 @@
-package String__21;
-
-public class Main {
-    public static void main(String[] args) {
-        char [] ch =  {'s' ,'h' , 'p' ,'a'};
-        String str = new String(ch);
-        System.out.println(str.length());
-    }
-}
