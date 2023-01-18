759b974ae0e2c889b9676d044ebc8f0c2a2dffc7
sai <saihlaingphyoaung03@gmail.com>
2023-01-18 13:21:14 +0630

    Revert "Revert "Revert "Revert "Revert "Revert "Revert "Revert "Revert "Revert "Revert "Revert "oop lessons""""""""""""
    This reverts commit 1acdbe0565ca82a84d2863aff0fe849353aafada.

---
+++ src/Abstract__14/Car.java
@@ -0,0 +1,11 @@
+package Abstract__14;
+
+public class Car extends Machine{
+    @Override
+    public void start() {
+        System.out.println("Car started");
+    }
+    public void stop() {
+        System.out.println("car stopped");
+    }
+}
