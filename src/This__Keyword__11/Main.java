b69c3b5b85fc8aa8624e8e4b642f0b3e49c59bd7
sai <saihlaingphyoaung03@gmail.com>
2023-01-18 13:22:18 +0630

    Revert "Revert "Revert "Revert "Revert "Revert "Revert "Revert "Revert "Revert "Revert "Revert "Revert "Revert "Revert "Revert "Revert "Revert "Revert "Revert "Revert "Revert "Revert "Revert "Revert "oop lessons"""""""""""""""""""""""""
    This reverts commit 5e6ca2ba484df8b8dbb6a8b36afb2e6b82209957.

--- src/This__Keyword__11/Main.java
+++ src/This__Keyword__11/Main.java
@@ -1,15 +1,2 @@
-package This__Keyword__11;
-
-import java.rmi.StubNotFoundException;
-
-public class Main {
-    public static void main(String[] args) {
-        Student sd = new Student();
-        sd.name = "mgmg";
-        sd.showName("Ma ma");
-
-        Student sd1 = new Student();
-        sd1.name = "haha";
-        sd1.showName("zaw zaw");
-    }
+package This__Keyword__11;public class Main {
 }
